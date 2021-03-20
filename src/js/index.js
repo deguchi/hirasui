afaa033f3eb39a9ddf028a4f4f56bb81e6236cbd
deguchi <deguchik@gmail.com>
2021-03-20 16:39:06 +0900

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,24 @@
-console.log('Hello LooselyTemplate!')
+var sliderImages = document.querySelectorAll('.slider img');
+var imageCount = sliderImages.length;
+var count = 0;
+
+function addAnimationClass() {
+    Array.prototype.slice.call(sliderImages).map(function(sliderImage, index) {
+        if (count===index) {
+            sliderImage.classList.add('animation');
+            setTimeout(function() {
+                sliderImage.classList.remove('animation');
+            }, 8000);
+        }
+    });
+    if (count===imageCount - 1) {
+        count = 0;
+    } else {
+        count += 1;
+    }
+}
+
+setTimeout(function() {
+    addAnimationClass();
+    setInterval(addAnimationClass, 6000);
+}, 1500);
